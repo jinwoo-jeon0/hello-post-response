4e80f5d51fd19480e519ef469efb21c65ff460cf
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2024-05-05 17:29:45 +0900

    Increase font size

--- app/foo/layout.tsx
+++ app/foo/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="m-8 flex space-x-2">
+    <div className="m-8 flex space-x-2 text-xl">
       {children}
     </div>
   );
